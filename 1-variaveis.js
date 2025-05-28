bc98d83ba5e3db9b18c3708bbd6a4c96d5db334c
segato.tiago <segato.tiago@gmail.com>
2025-05-28 11:54:02 +0000

    atualizações em variáveis, adição de operadores e renomeação dos arquivos

--- 1-variaveis.js
+++ 1-variaveis.js
@@ -1,17 +1,42 @@
-/* alert('Hello, World!');
+//Primeiro script - Libertando da maldição!
+//console.log('Hello, World!');
 
-var message = 'Hello, World!';
-alert(message);
+//var message = 'Hello, World!';
+//console.log(message);
+
+/*
+var n1, n2
+n1 = 5
+n2 = 2
+console.log(n1 + n2)
+*/
+
+//Identificadores:
+/*
+Podem começar com letra, $, ou _
+Não pode começar com números
+Pode combinar letras e números
+É possível usar símbolos e acentos
+Não pode conter espaços
+Não podem ser palavras reservadas
+Maiúsculas e minúsculas são diferentes
+Usar nomes coerentes
+*/
 
 //const ou let
 
+/*
 let message = 'Hello, World!';
 message = 'Nice to meet you!'; //com const não seria possível
-alert(message);
-
+console.log(message);
 */
 
 //Strings
+/*
+"Tiago"
+'Segato'
+`javascript`
+*/
 
 /*
 const nome = 'Tiago'
@@ -22,11 +47,12 @@ const sobrenome = 'Segato'
 console.log('Eu sou: ' + nome + ' ' + sobrenome)
 console.log(`Eu sou: ${nome} ${sobrenome}`) //template string
 
-console.log(nome.toLocaleLowerCase())
+console.log(nome.toLowerCase())
 console.log(sobrenome.toUpperCase())
  */
 
 //Numbers
+//7, 3.14, -15, -0.5
 
 /*
 const numero = 7 
@@ -39,6 +65,7 @@ console.log(typeof numero)
 */
 
 //Boolean
+//true, false
 
 /*
 console.log(true)
@@ -51,4 +78,15 @@ console.log(1 > 2)
 /*
 const x = null //esse está vazio
 const y = undefined //esse não foi definido
+*/
+
+//Conversões de Tipo
+
+/*
+Number.parseFloat(n)
+Number.parseInt(n)
+Number(n)
+
+n.toString()
+String(n)
 */
