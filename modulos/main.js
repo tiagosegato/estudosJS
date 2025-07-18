f8ee57ba82711f0689f400e1ebcdb27713e8391c
Tiago <tiago.segato@ifb.edu.br>
2025-07-18 16:51:21 -0300

    Adicionando exemplo de módulos

---
+++ modulos/main.js
@@ -0,0 +1,7 @@
+import * as Utils from './utils.js'
+
+console.log(Utils.somar(7, 2))
+
+console.log(Utils.multiplicar(5, 3))
+
+console.log(Utils.PI)
---
+++ modulos/utils.js
@@ -0,0 +1,11 @@
+//Criando as funções úteis
+
+export function somar(a, b) {
+    return a + b
+}
+
+export function multiplicar(a, b) {
+    return a * b
+}
+
+export const PI = 3.1416
