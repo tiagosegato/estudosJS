1d7137840ece5c5bf3616b3c7d6e436c7481c695
tiagosegato <tiago.segato@ifb.edu.br>
2025-07-31 09:51:28 -0300

    lista e objetos finalizado

--- 6-listas.js
+++ 6-listas.js
@@ -1,21 +1,27 @@
 //Arrays (vetor) - Variáveis compostas
 /*
 let num = [1, 2, 3, 4, 5];
-console.log(num);
 
-console.log(num[0]); //mostra o primeiro item
+console.log(num);
+console.log(num[0]);
 
 //adicionando elementos no array
 num[5] = 6;
-num.push(6);
+console.log(num);
 
+num.push(7);
+console.log(num);
+*/
+
+/*
 //Manipulando as listas
 const nomes = ['Tiago', 'Segato', 'Silva', 10, false]
 
 console.log(nomes[1]) //acessa o item x da lista
 console.log(nomes.length) //tamanho da lista
 
-nomes[2] = "Le" //altera o item x da lista
+nomes[2] = "Alessandra" //altera o item x da lista
+console.log(nomes)
 
 nomes.push('Ferro') //adiciona um item no final lista
 console.log(nomes)
@@ -34,18 +40,24 @@ console.log(nomes)
 
 console.log(nomes.indexOf('Segato')) //retorna o índice do item na lista
 
+//Para saber mais (MDN):
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array
+*/
 
+/*
 //Executando a lista num laço de repetição
-let num = [1, 2, 3, 4, 5];
+let num = [10, 20, 30, 40, 50];
 for (let pos = 0; pos < num.length; pos++) {
-    console.log(num);
+    console.log(num[pos]);
 }
 
 //ou...
-for (let pos of num) {
-    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+for (let pos of num) { //pos acessa os valores diretamente
+    console.log(pos)
 }
+*/
 
+/*
 //forEach
 let compras = [10, 1.99, 100, 20.12];
 let total = 0
@@ -57,32 +69,36 @@ compras.forEach(function (valor) { //Função de callback
 //compras.forEach(valor => total += valor); função como arrow function
 
 console.log(`A soma das compras é ${total.toFixed(2)}`)
-
-
-//map, filter e reduce
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-//Usando map
-const numerox2 = numbers.map(function (number) {
-    return number * 2 // A diferença para o forEach é que o map tem o return
-})
-
-//const numerox2 = numbers.map((num) => num * 2);// versão com arrow functions
-
-console.log(numerox2)
-
-//Usando filter
-const ages = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
-
-const evenAges = ages.filter(function (age) {
-    return age % 2 === 0 // retorna um novo array com itens baseados na condição (idades pares)
-})
-console.log(evenAges)
-
-//Usando reduce
-const sumAges = ages.reduce(function (sum, age) { //sempre vai ter um acumulador (sum) e o valor atual (age)
-    return sum + age //reduz o array em um único valor
-})
-console.log(sumAges)
 */
+
+/*
+//map, filter e reduce
+const idades = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+
+//Usando map: Transforma cada elemento e retorna um novo array com os resultados (mesmo tamanho do original).
+const idadex2 = idades.map(function (idade) {
+    return idade * 2 // A diferença para o forEach é que o map tem o return
+})
+
+//const idadex2 = idades.map((idade) => idade * 2);// versão com arrow functions
+
+console.log(idadex2)
+
+//Usando filter: Filtra elementos com base em uma condição e retorna um novo array só com os que atendem (pode ser menor).
+const idadePar = idades.filter(function (idade) {
+    return idade % 2 === 0 // retorna um novo array com itens baseados na condição (idades pares)
+})
+console.log(idadePar)
+
+//Usando reduce: Reduz o array a um único valor acumulando resultados (ex.: soma total).
+const somaIdades = idades.reduce(function (soma, idade) { //sempre vai ter um acumulador (soma) e o valor atual (idade)
+    return soma + idade //reduz o array em um único valor
+})
+console.log(somaIdades)
+*/
+
+//RESUMINDO...
+// map → Transforma e mantém o tamanho.
+// filter → Seleciona e pode reduzir o tamanho.
+// reduce → Agrega em um valor único (mais flexível, mas complexo).
 
--- 7-objetos.js
+++ 7-objetos.js
@@ -4,100 +4,98 @@
 // Em JavaScript, objetos são criados usando chaves {} e podem conter propriedades e métodos.
 // Propriedades são os dados armazenados no objeto, enquanto métodos são funções associadas ao objeto.
 // Exemplo de criação de um objeto simples
+const animal = {
+    nome: 'Bob',
+    especie: 'Cachorro',
+    idade: 3
+}
 
-// const animal = {
-//     nome: 'Bob',
-//     especie: 'Cachorro',
-//     idade: 3
-// }
+/*
+console.log(animal); //mostra o objeto inteiro
 
-// console.log(animal); //mostra o objeto inteiro
+//acessando as propriedades do objeto por: notação de ponto
+console.log(animal.nome);
+console.log(animal.especie);
+console.log(animal.idade);
 
-// //acessando as propriedades do objeto por: notação de ponto
-// console.log(animal.nome);
-// console.log(animal.especie);
-// console.log(animal.idade);
-
-// //ou
-
-// console.log(`O animal ${animal.nome}, é um ${animal.especie}, e tem ${animal.idade} anos. `)
+//ou
+console.log(`O animal ${animal.nome}, é um ${animal.especie}, e tem ${animal.idade} anos. `)
 
 //acessando as propriedades do objeto por: notação de colchetes
-// console.log(animal['nome']);
+console.log(animal['nome']);
+*/
 
-// //Acessando os dados por meio de função
-// function exibeDadosAnima(objAnimal, dado) {
-//     return objAnimal[dado];
-// }
+/*
+//Acessando os dados por meio de função
+function exibeDadosAnimal(objeto, dado) {
+    return objeto[dado];
+}
 
-// //chamando a função
-// console.log(exibeDadosAnima(animal, 'nome'))//precisa passar o objeto e o valor da chave que deseja acessar
-// console.log(exibeDadosAnima(animal, 'especie'));
+//chamando a função
+console.log(exibeDadosAnimal(animal, 'nome'))//precisa passar o objeto e o valor da chave que deseja acessar
+console.log(exibeDadosAnimal(animal, 'especie'));
 
 //se você tentar acessar uma propriedade que não existe, o resultado será undefined
-//console.log(animal.peso); //undefined
+console.log(animal.peso); //undefined
 
-// //adicionando um novo item ao objeto
-// animal.peso = 10;
-// console.log(animal.peso);
+//adicionando um novo item ao objeto
+animal.peso = 10;
+console.log(animal.peso);
+console.log(animal);
 
-// //alterando um item do objeto
-// animal.idade = 4;
-// console.log(animal.idade);
+//alterando um item do objeto
+animal.idade = 4;
+console.log(animal.idade);
+console.log(animal);
 
-// //removendo um item do objeto
-// delete animal.especie;
-// console.log(animal);
+//removendo um item do objeto
+delete animal.especie;
+console.log(animal);
+*/
 
-// //criando um objeto vazio
-// const carro = {};
-// console.log(carro);
+/*
+//criando um objeto vazio
+const carro = {};
+console.log(carro);
 
-// carro.modelo = 'Fusca'
-// console.log(carro)
+carro.modelo = 'Fusca'
+console.log(carro)
+*/
 
+/*
 //Percorrendo um objeto (for..in)
-// for (let chave in animal) {
-//     console.log(`${chave}: ${animal[chave]}`);
-// }
+for (let chave in animal) {
+    console.log(`${chave}: ${animal[chave]}`);
+}
+*/
 
-
-//OBJETOS COMPOSTOS
+// OBJETOS COMPOSTOS
 // Objetos podem conter outros objetos, formando estruturas mais complexas.
 // Exemplo de um objeto com outros objetos ou arrays dentro dele 
 
-// criando o objeto
-// const pessoa = {
-//     nome: "Tiago",
-//     sobrenome: "Segato",
-//     idade: 38,
-//     hobbies: ["futebol", "música", "programação"],
-//     endereco: {
-//         rua: "Rua Central",
-//         numero: 127,
-//         complemento: "Apto 101"
-//     }
-// }
-
-// //acessando um item da lista de hobbies
-// console.log(pessoa.hobbies[1]); //música
-
-// //acessando um item do objeto dentro do objeto
-// console.log(pessoa.endereco.rua); //Rua Central
-
-// //???
-// // const nome = pessoa.nome;
-// // const sobrenome = pessoa.sobrenome;
-// // const idade = pessoa.idade;
-// // const hobbies = pessoa.hobbies;
-
-// //equivalente ao código acima
-// const {nome, sobrenome, idade, hobbies} = pessoa;
-
-// console.log(nome, sobrenome, idade, hobbies);
-
-//CRIANDO UMA LISTA COM VÁRIOS OBJETOS
 /*
+// criando o objeto "complexo"
+const pessoa = {
+    nome: "Tiago",
+    sobrenome: "Segato",
+    idade: 38,
+    hobbies: ["futebol", "música", "programação"],
+    endereco: {
+        rua: "Rua Central",
+        numero: 127,
+        complemento: "Apto 101"
+    }
+}
+
+//acessando um item da lista de hobbies
+console.log(pessoa.hobbies[1]);
+
+//acessando um item do objeto dentro do objeto
+console.log(pessoa.endereco.rua);
+*/
+
+/*
+//CRIANDO UMA LISTA COM VÁRIOS OBJETOS
 const todos = [
     {
         id: 1,
@@ -123,16 +121,17 @@ console.log(todos[2].texto);
 //OBJETOS COM FUNÇÕES
 // Objetos podem conter funções, que são chamadas de métodos.
 // Métodos são funções associadas a um objeto e podem acessar as propriedades do objeto usando a palavra-chave `this`.
+/*
 // Exemplo de um objeto com um método   
-// const estudante = {
-//     nome: "Tiago",
-//     idade: 38,
-//     curso: "JavaScript",
-//     media: 7,
-//     estaAprovado: function () {
-//         return this.media >= 6 ? true : false;
-//     }
-// }
+const estudante = {
+    nome: "Tiago",
+    idade: 38,
+    curso: "JavaScript",
+    media: 7,
+    estaAprovado: function () {
+        return this.media >= 6 ? true : false;
+    }
+}
 
-// console.log(estudante.estaAprovado()); //true
-
+console.log(estudante.estaAprovado());
+*/
