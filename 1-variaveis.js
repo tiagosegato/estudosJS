25811d050bc5e18e7c009e5001fbd63c2835901c
tiagosegato <tiago.segato@ifb.edu.br>
2026-05-28 14:15:10 -0300

    update variáveis

--- 1-variaveis.js
+++ 1-variaveis.js
@@ -1,49 +1,83 @@
 //Primeiro script - Libertando da maldição!
 // console.log('Hello, World!');
 
+//VARIÁVEIS - var, let e const
+
 /*
 var message = 'Hello, World!';
 console.log(message);
 
-var n1, n2
+var idade = 25 //declarando
+console.log(idade) //usando
+
+idade = 26 //reatribuindo
+console.log(idade)
+
+var n1, n2 //declarando várias variáveis
 n1 = 5
 n2 = 2
 console.log(n1 + n2)
 */
 
-//Identificadores:
+//var - EVITAR
+/*
+var nome = "João" //declaração
+var nome = "Maria" //redeclaração permitida
+
+nome = "Pedro" //reatribuição permitida
+
+console.log(nome)
+*/
+
+//let - valores variáveis
+/*
+let salario = 5000
+// let salario = 4000 //ERRO - não redeclara!
+
+salario = 7000 // OK - reatribuição!
+
+console.log(salario)
+*/
+
+//const - valores constantes
+/*
+const PI = 3.1416
+//PI = 3.14 //ERRO - não reatribui
+
+console.log(PI)
+*/
+
+//EXEMPLO: Escopo de Bloco
+/*
+let message = 'Hello, World!';
+message = 'Welcome!'; //com const não seria possível
+console.log(message);
+
+//Exemplo com a let sendo usada em diferentes contextos/escopos
+let mensagem = 'Olá, Mundo!';
+{
+    let mensagem = 'Bem-vindo!'; //com const tambéms seria possível!
+}
+console.log(mensagem);
+*/
+
+
+//REGRAS para Identificadores:
 /*
 Podem começar com letra, $, ou _
 Não pode começar com números
 Pode combinar letras e números
-É possível usar símbolos e acentos
 Não pode conter espaços (_ ou camelCase)
 Não podem ser palavras reservadas
 Maiúsculas e minúsculas são diferentes
-Usar nomes coerentes
-JS é Case Sensitive!!!
+É possível usar símbolos e acentos - NÃO RECOMENDADO
+JS é Case Sensitive
+Usar nomes coerentes!!!
 */
 
-//let
 
-/*
-let message = 'Olá, Mundo!';
-var message = 'Bem-vindo!'; //com const não seria possível
-console.log(message);
-
-
-//Exemplo com a let sendo usada em diferentes contextos
-let message = 'Olá, Mundo!';
-{
-    let message = 'Bem-vindo!'; //com const não seria possível
-
-}
-console.log(message);
-*/
-
-//Tipos de Dados:
-//String, Number (Infinity, NaN), Boolean, Null, Undefined, Object (Array), Function
-
+//TIPOS DE DADOS
+//String, Number (Infinity, NaN), Boolean, Null, Undefined, Object, Array e Function
 
 //Strings
 /*
@@ -52,65 +86,67 @@ console.log(message);
 `javascript`
 */
 
-//Strings
+//STRINGS
 /*
 let nome = "Tiago"
 let sobrenome = 'Segato'
 let cidade = `Fernandópolis`
 
-console.log(nome + sobrenome)
-console.log(nome, sobrenome)
+console.log(nome + sobrenome) // aqui o + concatena
+console.log(nome, sobrenome) // e a , separa
+
 console.log("O nome é: " + nome + " e o sobrenome é: " + sobrenome)
 console.log(`O nome é: ${nome} e o sobrenome é: ${sobrenome}`)
 
 let textão = `vou colocar um texto
-em várias linhas`
+em várias linhas, só permitido com crase`
 
-//Funções
-
+//Funções para Strings
 console.log(nome.length)
 console.log(nome.toLowerCase())
 console.log(sobrenome.toUpperCase())
 */
 
-// mais funções em MDN...
-//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String#m%C3%A9todos
+//Funções para Strings (MDN)
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String
 
-//Numbers
+//NUMBERS
 // 7, 3.14, -15, -0.5
 
 /*
-let idade = 38
-let preco = 1.99
-let valor = 10 / 0
+let idade = 38      //Inteiro
+let preco = 1.99    //Decimal
+let valor = 10 / 0  //Infinity
+
+console.log(idade, preco, valor)
+console.log("texto" * 2); //NaN
 
 console.log(`O valor do produto é ${preco}`)
-console.log(typeof preco)
-console.log("texto" * 2);
+console.log(typeof preco) //para verificar os tipos de dados
 
 console.log(idade + 5)
-console.log(typeof idade.toString())
+console.log(typeof idade.toString()) //conversão de tipos
 
 console.log(typeof idade)
 */
 
 
-//Boolean
+//BOOLEAN
 //true, false
 
 /*
 let ativo = true
 let logado = false
 
+// Resultados de comparações são Booleanos
 let idade = 28
-
 let maiorIdade = idade >= 18
 
-console.log(maiorIdade)
+console.log(maiorIdade) // true
 */
 
 
-//Null e Undefined
+//NULL e UNDEFINED
 
 /*
 const x = null //esse está vazio
@@ -126,8 +162,44 @@ console.log(usuario)
 console.log(senha)
 */
 
+//OBJECT
+/*
+const carro = {
+    marca: "Fusca",
+    modelo: "Wolkswagen",
+    ano: 69,
+    acelerar: function () {
+        console.log("Vrumm Vrumm")
+    }
+}
 
-//Conversões de Tipo
+console.log(carro)
+console.log(carro.marca)
+carro.acelerar()
+*/
+
+//ARRAY
+/*
+const frutas = ["pera", "uva", "maça"]
+
+console.log(frutas)
+console.log(frutas[1])
+console.log(frutas.length)
+
+//Funções para Arrays (MDN)
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+*/
+
+//FUNCTION
+/*
+function somar(n1, n2) {
+    return (n1 + n2)
+}
+
+console.log(somar(3, 4))
+*/
+
+//CONVERSÕES DE TIPOS
 /*
 let num = "10"
 let numero = Number(num)
@@ -143,7 +215,15 @@ console.log(typeof valor)
 Number.parseFloat(n)
 Number.parseInt(n)
 Number(n)
++"42" //Number (atalho)
 
 n.toString()
 String(n)
+
+Boolean("texto")  // true
+Boolean("")       // false
+!!"ok"            // true (atalho)
+
+String(true);     // "true"
+String(false);    // "false"
 */
